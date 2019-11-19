88fd6fd77458b202231a90b4abe685cb6a126829
Vittorio Trettenero <vittotre@gmail.com>
2019-11-19 13:26:29 +0000

    Added angles processor to get mean of two images

--- src/angles_processor.py
+++ src/angles_processor.py
@@ -1,4 +1,16 @@
-from . import image1, image2
+#!/usr/bin/env python
+
+import image1
+import image2
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
 
 class Server:
     def __init__(self):
@@ -15,12 +27,17 @@ class Server:
 
     def compute_average(self):
         if self.angles1 is not None and self.angles2 is not None:
-            print("Angles1: ", self.angles1)
-            print("Angles2: ", self.angles2)
+            print("Angles1: ", self.angles1.data)
+            print("Angles2: ", self.angles2.data)
+	    print("Average: ", np.array([np.mean([i, j]) for (i, j) in zip(self.angles1.data, self.angles2.data)]))
+	    print()	
+	
 
 if __name__ == "__main__":
     server = Server()
 
     rospy.init_node('angles')
-    rospy.Subscriber("/joints_pos1", Float64MultiArray, server.image1_callback())
-    rospy.Subscriber("/joints_pos2", Float64MultiArray, server.image2_callback())
+    rospy.Subscriber("/joints_pos1", Float64MultiArray, server.image1_callback)
+    rospy.Subscriber("/joints_pos2", Float64MultiArray, server.image2_callback)
+
+    rospy.spin()
